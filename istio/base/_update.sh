6383ccf0f0ab0346cf0271bb11811d3add1bdc8f
Simone <simone@simone.sh>
2021-02-24 21:24:40 +0100

    Add istio

---
+++ istio/base/_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+VERSION=1.9.0
+ARCH=x86_64
+#ARCH=arm64
+
+DIR=$(pwd)
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$VERSION TARGET_ARCH=$ARCH sh -
+cd istio-$VERSION
+
+helm template --no-hooks istio-operator manifests/charts/istio-operator \
+  --set operatorNamespace="istio-operator" \
+  --set watchedNamespaces="istio-system" > $DIR/istio-operator.yml
+echo $(pwd)/istio-operator.yml
+rm -rf "$TMPDIR/istio-$VERSION/"
