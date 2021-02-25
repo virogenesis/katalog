32a896d46b8d78c8226a51ffe1d46aae7b6154ec
Simone <simone@simone.sh>
2021-02-25 15:47:56 +0100

    knative: Enable external-dns and cert-manager support

--- knative/base/_update.sh
+++ knative/base/_update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -ex
 VERSION="v0.20.0"
-curl -L "https://github.com/knative/operator/releases/download/$VERSION/operator.yaml" > knative-operator.yml
+curl -L "https://github.com/knative/operator/releases/download/$VERSION/operator.yaml" > operator/knative-operator.yml
+curl -L "https://github.com/knative/net-istio/releases/download/$VERSION/net-istio.yaml" > istio/net-istio.yml
+curl -L "https://github.com/knative/serving/releases/download/$VERSION/serving-default-domain.yaml" > xip.yml
+curl -L "https://github.com/knative/net-certmanager/releases/download/$VERSION/release.yaml" > cert-manager/net-certmanager.yml
+curl -L "https://github.com/knative/serving/releases/download/$VERSION/serving-nscert.yaml" > cert-manager/serving-nscert.yml
