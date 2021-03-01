14a52d9b3dcd2857443d5cc03e48073fca492566
Simone <simone@simone.sh>
2021-03-01 12:42:10 +0100

    Add ambassador

---
+++ ambassador/base/_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+VERSION=6.5.19
+
+helm repo add datawire https://www.getambassador.io || true
+
+helm template --no-hooks --include-crds \
+    --version=$VERSION \
+    --namespace ambassador \
+    --set test-enabled="false" \
+    --set namespace.name="ambassador" \
+    --set resources.requests.memory="100Mi" \
+    --set resources.requests.cpu="100m" \
+    ambassador datawire/ambassador > ambassador.yml
