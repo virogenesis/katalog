64d8188b8aafa4f0d7eeb7ed103d3c092bf5200f
Simone <simone@simone.sh>
2021-03-02 00:49:03 +0100

    knative: Update to 0.21.0

--- knative/base/_update.sh
+++ knative/base/_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-VERSION="v0.20.0"
+VERSION="v0.21.0"
 curl -L "https://github.com/knative/operator/releases/download/$VERSION/operator.yaml" > operator/knative-operator.yml
 curl -L "https://github.com/knative/net-istio/releases/download/$VERSION/net-istio.yaml" > istio/net-istio.yml
 curl -L "https://github.com/knative/serving/releases/download/$VERSION/serving-default-domain.yaml" > xip.yml
--- knative/contour/_update.sh
+++ knative/contour/_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=v0.20.0
+VERSION=v0.21.0
 
 curl -L "https://github.com/knative/net-contour/releases/download/$VERSION/net-contour.yaml" > net-contour.yml
