4702d54a0fd085b0f87d691ac8ba0655a6c5e505
Simone <simone@simone.sh>
2021-03-02 01:49:46 +0100

    contour: Use knative version

--- contour/base/_update.sh
+++ contour/base/_update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -ex
 
-VERSION=v1.13.0
-
-curl -L "https://projectcontour.io/quickstart/$VERSION/contour.yaml" > contour.yml
+VERSION=v0.21.0  # knative
+curl -L "https://github.com/knative-sandbox/net-contour/releases/download/$VERSION/contour.yaml" > contour.yml
