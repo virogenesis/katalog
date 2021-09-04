1bafa30111ff77d3f566e033f6505ea0f55a6cb7
Simone <simone@simone.sh>
2021-09-04 13:26:57 +0200

    Update kube-prometheus

--- kube-prometheus/base/_update.sh
+++ kube-prometheus/base/_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-VERSION=release-0.8
+VERSION=release-0.9
 kustomize build "github.com/prometheus-operator/kube-prometheus?ref=$VERSION" > upstream-kube-prometheus.yml
 kustomize build "github.com/sim1/kube-metrics-adapter?ref=master" > upstream-kube-metrics-adapter.yml
